95823012c85f3226ed1c3774611ac162f85ab5cd
calebliljeros-hash <caleb.liljeros@gmail.com>
2026-03-05 21:02:08 -0500

    Remove all starter project files.

--- Starter/src/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(
-    `Dependency auditing starter API running at http://localhost:${port}`
-  );
-});
